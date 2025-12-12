283d1ea255123fb30521e3029b4aa0ea48b4f15e
513609 <513609jb@eur.nl>
2025-12-12 16:10:16 +0100

    fix(apl): correct upstash configuration API

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -9,17 +9,27 @@ import { FileAPL } from "@saleor/app-sdk/APL/file";
  * To read more about storing auth data, read the
  * [APL documentation](https://github.com/saleor/saleor-app-sdk/blob/main/docs/apl.md)
  */
+import { UpstashAPL } from "@saleor/app-sdk/APL/upstash";
+
+/**
+ * By default auth data are stored in the `.auth-data.json` (FileAPL).
+ * For multi-tenant applications and deployments please use UpstashAPL.
+ *
+ * To read more about storing auth data, read the
+ * [APL documentation](https://github.com/saleor/saleor-app-sdk/blob/main/docs/apl.md)
+ */
 export let apl: APL;
 
-switch (process.env.APL) {
-  /**
-   * Depending on env variables, chose what APL to use.
-   * To reduce the footprint, import only these needed
-   *
-   * TODO: See docs
-   */
-  default:
-    apl = new FileAPL();
+if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+  console.log("✅ UpstashAPL selected. KV_REST_API_URL is found.");
+  apl = new UpstashAPL({
+    restURL: process.env.KV_REST_API_URL,
+    restToken: process.env.KV_REST_API_TOKEN,
+  });
+} else {
+  console.warn("⚠️ FileAPL selected. KV_REST_API_URL was NOT found.");
+  console.log("Env check: URL exists?", !!process.env.KV_REST_API_URL, "Token exists?", !!process.env.KV_REST_API_TOKEN);
+  apl = new FileAPL();
 }
 
 export const saleorApp = new SaleorApp({
